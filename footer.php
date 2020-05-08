35e635b5b649c11f7c6a8a86bf4abd9a7b771468
kanyeceejayA <33685273+kanyeceejayA@users.noreply.github.com>
2020-05-08 21:08:27 +0300

    add files

---
+++ footer.php
@@ -0,0 +1,22 @@
+<footer class="footer footer-default">
+    <div class="container">
+      <nav class="float-left">
+        <ul>
+          <li>
+            rtl.ug
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright float-right">
+        &copy;
+        <script>
+          document.write(new Date().getFullYear())
+        </script>
+      </div>
+    </div>
+  </footer>
+  
+</body>
+
+</html>
